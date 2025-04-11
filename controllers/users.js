9bbbabb85eb06bae66e0e6320d9fdfa8b420666f
tslater4 <tenoslater@gmail.com>
2025-04-10 20:07:11 -0500

    fixed issue where /user and /user/id did the same thing as one another

--- controllers/users.js
+++ controllers/users.js
@@ -21,7 +21,7 @@ router.get('/', verifyToken, async (req, res) => {
 router.get('/:userId', verifyToken, async (req, res) => {
     console.log(req.user);
   try {
-    const user = await User.find({}, "username recipes");
+    const user = await User.findById(req.params.userId);
     if (!user) {
         return res.status(404).json({ err: 'User not found' });
     }
